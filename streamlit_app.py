36072e964fa60d3b7f64c05f02e9c3cd1e5b9413
SeongHwan Kim <33649575+fwany21@users.noreply.github.com>
2025-02-14 11:11:36 +0000

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-from PIL import Image
+from PIL import Image, ExifTags
 
 # Title of the invitation
 st.title("🎉 나연이의 8번째 생일 파티에 초대합니다! 🎉")
@@ -19,6 +19,29 @@ st.write("""
 함께 와서 맛있는 거 먹고 즐겁게 놀아보아요~🤩
 """)
 
+# Function to correct image orientation
+def correct_image_orientation(image):
+    try:
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == 'Orientation':
+                break
+
+        exif = image._getexif()
+
+        if exif is not None:
+            orientation = exif.get(orientation)
+
+            if orientation == 3:
+                image = image.rotate(180, expand=True)
+            elif orientation == 6:
+                image = image.rotate(270, expand=True)
+            elif orientation == 8:
+                image = image.rotate(90, expand=True)
+    except Exception as e:
+        pass
+
+    return image
+
 # Photo slideshow
 photo_dir = 'photos'
 if os.path.exists(photo_dir):
@@ -27,9 +50,10 @@ if os.path.exists(photo_dir):
         st.write("## 📸 사진 슬라이드쇼")
         for photo in photos:
             image = Image.open(os.path.join(photo_dir, photo))
+            image = correct_image_orientation(image)
             width, height = image.size
             if width > height:
-                st.image(image, use_container_width=True)
+                st.image(image, use_column_width=True)
             else:
                 st.image(image, width=300)
     else:
