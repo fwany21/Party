042d0930f63be4966f74839ce494470bd7b1bfda
SeongHwan Kim <33649575+fwany21@users.noreply.github.com>
2025-02-14 14:27:06 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,6 @@ st.markdown("""
     .block-container {
         flex: 1;
     }
-    .stAppHeader {
-            background-color: rgba(255, 255, 255, 0.0);  /* Transparent background */
-            visibility: hidden;  /* Ensure the header is visible */
-        }
     </style>
     """, unsafe_allow_html=True)
 
